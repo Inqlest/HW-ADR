6159dbc610b707e5a7e925ceebf5f7afe6f554e3
“Ilya <tsoi_ilya02@mail.ru>
2023-01-18 08:31:34 +0300

    new commit

--- asd.cpp
+++ asd.cpp
@@ -18,17 +18,15 @@ public:
         apartment_number = 1;
     }
 
-    static int get_output_addresses() {
+    static void output_addresses() {
         ifstream in("in.txt");
         if (in.is_open() == 0) {
             cout << "Не удалось открыть in.txt" << endl;
-            return 0;
         }
         ofstream out("out.txt");
         if (out.is_open() == 0) {
             cout << "Не удалось открыть out.txt" << endl;
             in.close();
-            return 0;
         }
         int size;
         string line;
@@ -41,6 +39,7 @@ public:
             in >> ad[i].house_number;
             in >> ad[i].apartment_number;
         }
+        out << size << endl;
         for (int i = size-1; i >= 0; i--) {
             line = ad[i].city + ", " + ad[i].street + ", " + to_string(ad[i].house_number) + ", " + to_string(ad[i].apartment_number) + "\n";
 
@@ -51,14 +50,12 @@ public:
         out.close();
         in.close();
         delete[] ad;
-        return size;
     }
 };
 
 int main() {
     setlocale(LC_ALL, "Russian");
-    Addresses address;
-    address.get_output_addresses();
+    Addresses::output_addresses();
 
     return 0;
 }
